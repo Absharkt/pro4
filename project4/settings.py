e67f1aa015745d1872ffa918e9ecea0f30345852
Absharkt <absharktabshar@gmail.com>
2022-03-21 22:37:24 +0530

    star added

--- project4/settings.py
+++ project4/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-+x!44j&g16u795fxyhm-)@#*!)meulaqur1!jyiwz$%2+yw&gy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
